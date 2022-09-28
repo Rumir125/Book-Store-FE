e08817b44aab4bb8d4f36098523101d68c310e2d
Amit Leto <amirus95@hotmail.com>
2022-09-28 15:21:53 +0200

    fix build, add env var backend_api

--- pages/constants/keys.ts
+++
@@ -1,2 +0,0 @@
-export const GET_BOOKS = "GET_BOOKS";
-export const GET_USERS = "GET_USERS";
--- src/keys/endpoints.ts
+++ src/keys/endpoints.ts
@@ -1,1 +1,4 @@
-export const BACKEND_BASE_URL = `http://localhost:${process.env.backendPort}`;
+// export const BACKEND_BASE_URL = `http://localhost:${process.env.backendPort}`;
+export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_API
+  ? process.env.NEXT_PUBLIC_BACKEND_API
+  : `http://localhost:${process.env.backendPort}`;
